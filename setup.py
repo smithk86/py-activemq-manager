2ed1f66a0382172e9c075e60d0b82bd757f46cef
Kyle Smith <ksmith@smc3.com>
2018-03-17 10:31:39 -0400

    add setup.py and exempt setup files

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='activemq-console-parser',
+    version='0.5.0',
+    license='MIT',
+    author='Kyle Smith',
+    author_email='smithk86@gmail.com',
+    description='gather information about activemq via the web console',
+    packages=['activemq_console_parser'],
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'lxml==4.1.1'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
