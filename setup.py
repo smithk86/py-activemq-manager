3c38249ca3e58d1eab96acf97fbe2df2d4caa772
Kyle Smith <ksmith@smc3.com>
2018-03-19 09:29:52 -0400

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='activemq-console-parser',
-    version='0.5.0',
+    version='0.5.1',
     license='MIT',
     author='Kyle Smith',
     author_email='smithk86@gmail.com',
