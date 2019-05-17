6900f2411617002d4427aeba0792596d7654041b
Kyle Smith <ksmith@smc3.com>
2019-05-17 13:14:33 -0400

    switch back to lxml

--- setup.py
+++ setup.py
@@ -4,15 +4,16 @@ from setuptools import setup
 
 setup(
     name='activemq-console-parser',
-    version='1.0.3',
+    version='1.1.0',
     license='MIT',
     author='Kyle Smith',
     author_email='smithk86@gmail.com',
     description='gather information about activemq via the web console',
     packages=['activemq_console_parser'],
     install_requires=[
-        'requests',
-        'beautifulsoup4'
+        'beautifulsoup4',
+        'lxml',
+        'requests'
     ],
     classifiers=[
         'Intended Audience :: Developers',
