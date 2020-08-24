8dd1587144d3f40107358cb28f1e78588f353f25
Kyle Smith <ksmith@smc3.com>
2020-08-24 12:22:53 -0400

    refactor Queue

--- activemq_manager/queue.py
+++ activemq_manager/queue.py
@@ -12,43 +12,38 @@ class Queue:
     def __init__(self, broker, name):
         self.broker = broker
         self.name = name
-        self.size = None
-        self.enqueue_count = None
-        self.dequeue_count = None
-        self.consumer_count = None
-
-    @staticmethod
-    async def new(broker, name):
-        q = Queue(broker, name)
-        await q.update()
-        return q
+        self._attributes = None
 
     def __repr__(self):
         return f'<activemq_manager.Queue object name={self.name}>'
 
+    @staticmethod
+    async def new(broker, name):
+        q = Queue(broker, name)
+        return await q.update()
+
     async def update(self):
-        data = await self.attribute([
-            'QueueSize',
-            'EnqueueCount',
-            'DequeueCount',
-            'ConsumerCount'
-        ])
-        self.size = data.get('QueueSize')
-        self.enqueue_count = data.get('EnqueueCount')
-        self.dequeue_count = data.get('DequeueCount')
-        self.consumer_count = data.get('ConsumerCount')
+        self._attributes = await self.attributes()
+        return self
 
-    def asdict(self):
-        return {
-            'name': self.name,
-            'size': self.size,
-            'enqueue_count': self.enqueue_count,
-            'dequeue_count': self.dequeue_count,
-            'consumer_count': self.consumer_count
-        }
+    @property
+    def size(self):
+        return self._attributes['QueueSize']
 
-    async def attribute(self, attribute_):
-        return await self.broker.api('read', f'org.apache.activemq:type=Broker,brokerName={self.broker.name},destinationType=Queue,destinationName={self.name}', attribute=attribute_)
+    @property
+    def enqueue_count(self):
+        return self._attributes['EnqueueCount']
+
+    @property
+    def dequeue_count(self):
+        return self._attributes['DequeueCount']
+
+    @property
+    def consumer_count(self):
+        return self._attributes['ConsumerCount']
+
+    async def attributes(self):
+        return await self.broker.api('read', f'org.apache.activemq:type=Broker,brokerName={self.broker.name},destinationType=Queue,destinationName={self.name}')
 
     async def purge(self):
         await self.broker.api('exec', f'org.apache.activemq:brokerName={self.broker.name},type=Broker,destinationType=Queue,destinationName={self.name}', operation='purge')
